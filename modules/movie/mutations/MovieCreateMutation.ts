de8a80a1074d6fa9e488bdc2d7eeca751ea12dfd
Eduardo Maciel <mrgamex49@gmail.com>
2021-07-16 20:47:59 -0300

    feat(createMovie): add mutation to create Movie

--- modules/movie/mutations/MovieCreateMutation.ts
+++ modules/movie/mutations/MovieCreateMutation.ts
@@ -1,29 +1,36 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLFloat, GraphQLObjectType, GraphQLResolveInfo, GraphQLString } from "graphql";
 import MovieType from "../MovieType";
-import MovieInputFilterType from "../MovieInputFilterType";
 import MovieModel from "../MovieModel"
-const createUserMutation = new GraphQLObjectType({
-    name: 'Create user',
-    description: "Create user mutation",
-    fields:{
-        addMovie: {
-            type: MovieType,
-            args: {
-                input: {
-                    type: MovieInputFilterType
-                }
-            },
-            resolve: (source, args) => {
-                let movie = new MovieModel({
-                    name: args.name,
-                    producer: args.producer,
-                    rating: args.rating
-                })
-                return movie.save()
-            }
-            
-        }
-    }
-})
 
-export default createUserMutation
+
+const addMovie =  {
+    type: MovieType,
+    args: {
+        name: {
+            type: GraphQLString,
+        },
+        producer: {
+            type: GraphQLString,
+        },
+        rating: {
+            type: GraphQLFloat,
+        }
+    },
+    resolve: async(root, args) => {
+        let movie = new MovieModel({
+            name: args.name,
+            producer: args.producer,
+            rating: args.rating
+        })
+        let newMovie = await movie.save();
+        if (!newMovie) {
+            throw new Error("Error to save new movie");
+        }
+        return newMovie;
+    }
+    
+}
+
+
+
+export default addMovie
